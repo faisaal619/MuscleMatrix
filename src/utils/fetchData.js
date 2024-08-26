feea5ac51786e3379c02b84aafc8c9791e5de6c3
faisaal619 <faisal.alasadi92@gmail.com>
2024-08-25 21:26:37 -0700

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,19 @@
+export const exerciseoptions = {
+	method: 'GET',
+	url: 'https://exercisedb.p.rapidapi.com/exercises?limit=0',
+	
+	headers: {
+	  'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+	  'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+  }
+ 
+}
+ 
+ export const fetchData = async (url, options) => {
+	
+   const response = await fetch (url, options);
+   const data = await response.json() ;
+
+   return data;
+
+ }
