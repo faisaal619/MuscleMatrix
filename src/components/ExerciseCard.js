bd7ddcd2b8e77e5edfce721911cb6a5909c9e8fc
faisaal619 <faisal.alasadi92@gmail.com>
2024-08-31 01:57:52 -0700

    #

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,37 +1,32 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import {Button, Stack, Typography} from '@mui/material';
+import { Button, Stack, Typography } from '@mui/material';
 
-const ExerciseCard = ({exercise}) => {
-  const scrollToTop = () =>{ 
-    window.scrollTo({ 
-      top: 0,  
-      behavior: 'smooth'
-    
-    }); 
-  }; 
+const scrollToTop = () =>{ 
+  window.scrollTo({ 
+    top: 0,  
+    behavior: 'smooth'
+    /* you can also use 'auto' behaviour 
+       in place of 'smooth' */
+  }); 
+}; 
 
-  return ( 
+const ExerciseCard = ({ exercise }) => (
 
- <Link className="exercise-card" to={`/exercise/${exercise.id}`}  onClick={scrollToTop} >
-     <img src={exercise.gifUrl} alt={exercise.name} loading="lazy"/>
+  <Link className="exercise-card" to={`/exercise/${exercise.id}`}  onClick={scrollToTop}>
+    <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
     <Stack direction="row">
-       <Button sx={{ml:'21px', color:'#fff', background: '#ffa9a9',
-       fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize'}}>
-         {exercise.bodyPart}
-       </Button>
-       <Button sx={{ml:'21px', color:'#fff', background: '#fcc757',
-       fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize'}}>
-         {exercise.target}
-       </Button>
+      <Button sx={{ ml: '21px', color: '#fff', background: '#FFA9A9', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+        {exercise.bodyPart}
+      </Button>
+      <Button sx={{ ml: '21px', color: '#fff', background: '#FCC757', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+        {exercise.target}
+      </Button>
     </Stack>
-    <Typography ml="21px" color="#000" fontWeight="bold" mt="11px" pb="10px" textTransform="capitalize">
-        {exercise.name}
-       </Typography>
+    <Typography ml="21px" color="#000" fontWeight="bold" sx={{ fontSize: { lg: '24px', xs: '20px' } }} mt="11px" pb="10px" textTransform="capitalize">
+      {exercise.name}
+    </Typography>
   </Link>
+);
 
-
-  )
-}
-
-export default ExerciseCard
+export default ExerciseCard;
