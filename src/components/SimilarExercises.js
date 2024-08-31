577e4cef94087b93008e813cf0e987c5589843ad
faisaal619 <faisal.alasadi92@gmail.com>
2024-08-30 17:15:21 -0700

    #

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -7,7 +7,7 @@ import Loader
 const SimilarExercises = ({targetMuscleExercises, equipmentExercises}) => {
   return (
    <Box sx={{mt: {lg:'100px', xs:'0'}}}>
-     <Typography variant="h3" mb={5}>Exercises that target the same muscle group </Typography>
+     <Typography variant="h3" mb={5}>Exercises that target the same musucle group </Typography>
      <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
       {targetMuscleExercises.length  ? <HorizontalScrollBar data={targetMuscleExercises} /> : <Loader />}
     </Stack>
