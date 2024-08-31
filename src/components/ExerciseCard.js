852ac2e14971ae877adbd6813cc3aa4e491e4707
faisaal619 <faisal.alasadi92@gmail.com>
2024-08-31 00:14:12 -0700

    #

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -3,8 +3,17 @@ import { Link } from 'react-router-dom';
 import {Button, Stack, Typography} from '@mui/material';
 
 const ExerciseCard = ({exercise}) => {
+  const scrollToTop = () =>{ 
+    window.scrollTo({ 
+      top: 0,  
+      behavior: 'smooth'
+      /* you can also use 'auto' behaviour 
+         in place of 'smooth' */
+    }); 
+  }; 
+
   return (
-  <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+  <Link className="exercise-card" to={`/exercise/${exercise.id}`} onClick={scrollToTop}>
      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy"/>
     <Stack direction="row">
        <Button sx={{ml:'21px', color:'#fff', background: '#ffa9a9',
